de31f3836f6ac7c794a00341ed0f04771422887b
Jill Warvel <jillo@mailjill.com>
2012-03-03 17:56:58 -0500

    test1

--- js/test1.js
+++ js/test1.js
@@ -1,3 +1,29 @@
 $(document).ready( function(){
+	//$('#Overlay1Div').overlay().close();
 	$('h1').css("color","blue");
+	//$('#click1Btn').click( function() {
+	
+	$('#Overlay1Div').overlay({
+		// custom top position
+		top: 260,
+		// some mask tweaks suitable for facebox-looking dialogs
+		mask: {
+
+			// you might also consider a "transparent" color for the mask
+			color: '#f22',
+
+			// load mask a little faster
+			loadSpeed: 200,
+
+			// very transparent
+			opacity: 0.5
+		},
+
+		// disable this for modal dialog-type of overlays
+		closeOnClick: false,
+
+		// load it immediately after the construction
+		load: true	
+
+	});
 });
