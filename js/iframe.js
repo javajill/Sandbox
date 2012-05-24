21f64f804252a140e4fea6607196d0fed57ad8c7
Jill Warvel <jwarvel@exacttarget.com>
2012-05-24 09:43:49 -0400

    iframe testing

---
+++ js/iframe.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    //$('#Overlay1Div').overlay().close();
+    $('h3').css("color", "blue");
+
+    $('p').click(function(e) {
+        alert("p clicked");
+        return false;
+    });
+
+
+});
---
+++ js/iframe2.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    //$('#Overlay1Div').overlay().close();
+    $('h3').css("color", "red");
+
+    $('p').click(function(e) {
+        alert("p clicked iframe2");
+        return false;
+    });
+
+
+});
