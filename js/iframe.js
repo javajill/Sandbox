8eff06961f7938b136b38d89e0aab8408ee022f0
Jill Warvel <jwarvel@exacttarget.com>
2012-05-28 11:19:26 -0400

    iframe testing

--- js/iframe.js
+++ js/iframe.js
@@ -1,4 +1,10 @@
+var test1 = "test1"
 $(document).ready(function() {
+    
+    
+    ifr =  document.getElementById("iframe2").contentDocument;
+    
+    
     //$('#Overlay1Div').overlay().close();
     $('h3').css("color", "blue");
 
@@ -7,5 +13,18 @@ $(document).ready(function() {
         return false;
     });
 
+    document.addEventListener("click",onContextMenu, true);  
+
+        $(ifr).find("p").live("click", function (e) {
+            e.preventDefault();
+            alert("click from iframe.js");
+        });
+
+    function onContextMenu(e){
+        alert("doc clk "+ test1);
+        e.preventDefault();
+        e.stopPropagation();
+        
+    }
 
 });
--- js/iframe2.js
+++ js/iframe2.js
@@ -1,11 +1,16 @@
+var test2 = "test2"
 $(document).ready(function() {
     //$('#Overlay1Div').overlay().close();
     $('h3').css("color", "red");
 
-    $('p').click(function(e) {
-        alert("p clicked iframe2");
+    $('xxp').click(function(e) {
+        alert("p clicked iframe2 " + "parent.test1 = " + parent.test1);
         return false;
     });
 
+//        target.find("*").andSelf().prop("onmouseover", null).prop("onchange", null).prop("onclick", null).prop("ondblclick", null).prop("onfocus", null).prop("onkeydown", null).prop("onkeypress", null).prop("onmouseover", null).prop("onkeypress", null).prop("onload", null).prop("onmousedown", null).prop("onmouseup", null).prop("onmousemove", null).prop("onmouseout", null).prop("onreset", null).prop("onselect", null).prop("onsubmit", null).prop("onunload", null);
+       $("*").andSelf().prop("onmouseover", null).prop("onchange", null).prop("onxxxclick", null).prop("ondblclick", null).prop("onfocus", null).prop("onkeydown", null).prop("onkeypress", null).prop("onmouseover", null).prop("onkeypress", null).prop("onload", null).prop("onmousedown", null).prop("onmouseup", null).prop("onmousemove", null).prop("onmouseout", null).prop("onreset", null).prop("onselect", null).prop("onsubmit", null).prop("onunload", null).prop("contextmenu", null);
+
+ //parent.$("*").trigger("click");
 
 });
